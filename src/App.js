ef107d345d5177333254f0a310621f7832c54f42
Nikhil Bokade <nikhilbokade25@gmail.com>
2024-02-12 17:03:44 -0500

    Project Initialization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+<<<<<<< HEAD
+=======
+
+>>>>>>> fbc2a9e (Project Initialization)
 import './App.css';
 import Navbar from "../src/Components/Navbar";
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <Navbar />
+=======
+      <h2>hello world!</h2>
+>>>>>>> fbc2a9e (Project Initialization)
     </div>
   );
 }
