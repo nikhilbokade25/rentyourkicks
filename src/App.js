5be53e8f303e613de1a546850927f8de44d96c7f
Nikhil Bokade <nikhilbokade25@gmail.com>
2024-02-12 20:17:37 -0500

    resolved navbar erros

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> fbc2a9e (Project Initialization)
 import './App.css';
 import Navbar from "../src/Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
       <Navbar />
-=======
-      <h2>hello world!</h2>
->>>>>>> fbc2a9e (Project Initialization)
+
+  
+
     </div>
   );
 }
