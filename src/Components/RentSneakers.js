bccd718ce8937d7ec242b50feeda7304274a1bf6
Nikhil Bokade <nikhilbokade25@gmail.com>
2024-02-17 18:42:40 -0500

    Implemented Rent Sneakers Component with responsive layout

--- src/Components/RentSneakers.js
+++ src/Components/RentSneakers.js
@@ -1,9 +1,25 @@
 import "./RentSneakers.css";
+import shopKidImg from "../assets/shopKid.jpg";
+import shopWomenImg from "../assets/shopWomen.jpg";
+import shopMenImg from "../assets/shopMan.jpg";
 
 export default function RentSneakers(){
     return(
         <div className="rent_container">
-
+            <div className="rent_main">
+                <div className="shop_kids shop">
+                    <img src={shopKidImg} alt="a kid in a sneaker image." className="rent_image"/>
+                    <p className="title">SHOP FOR KIDS</p>
+                </div>
+                <div className="shop_women shop">
+                <img src={shopWomenImg} alt="a woman tying sneaker image." className="rent_image"/>
+                <p className="title">SHOP FOR WOMEN</p>
+                </div>
+                <div className="shop_men shop">
+                <img src={shopMenImg} alt="a man in a sneaker image." className="rent_image"/>
+                <p className="title">SHOP FOR MEN</p>
+                </div>
+            </div>
         </div>
     );
 }
